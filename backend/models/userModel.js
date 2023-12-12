5eb34a4ab0346d21417bc500153820fa56f8d35a
Talatizi <talatizikamel88@gmail.com>
2023-12-12 14:13:29 +0100

    le hashage du mot du passe

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const dbConfig = require("../dbConfig");
+const bcrypt = require("bcrypt");
 
 const connection = mysql.createConnection(dbConfig);
 
@@ -11,38 +12,52 @@ const User = {
   signin: (userData, callback) => {
     const { nom, prenom, email, password } = userData;
 
-    // je vérifie d'abord si l'e-mail existe déjà dans la base de données
-    connection.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email],
-      (err, results) => {
-        if (err) {
-          console.error("Erreur lors de la vérification de l'e-mail :", err);
-          callback(err, null);
-        } else if (results.length > 0) {
-          const error = new Error("L'utilisateur avec cet e-mail existe déjà.");
-          callback(error, null);
-        } else {
-          const sql =
-            "INSERT INTO users (nom, prenom, email, password) VALUES (?, ?, ?, ?)";
-          connection.query(
-            sql,
-            [nom, prenom, email, password],
-            (insertErr, result) => {
-              if (insertErr) {
-                console.error(
-                  "Erreur lors de l'insertion de l'utilisateur dans la base de données :",
-                  insertErr
-                );
-                callback(insertErr, null);
-              } else {
-                callback(null, result);
-              }
+    // Hash du mot de passe
+    bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
+      if (hashErr) {
+        console.error("Erreur lors du hachage du mot de passe :", hashErr);
+        callback(hashErr, null);
+      } else {
+        // je vérifie d'abord si l'e-mail existe déjà dans la base de données
+        connection.query(
+          "SELECT * FROM users WHERE email = ?",
+          [email],
+          (err, results) => {
+            if (err) {
+              console.error(
+                "Erreur lors de la vérification de l'e-mail :",
+                err
+              );
+              callback(err, null);
+            } else if (results.length > 0) {
+              const error = new Error(
+                "L'utilisateur avec cet e-mail existe déjà."
+              );
+              callback(error, null);
+            } else {
+              // Insertion de l'utilisateur avec le mot de passe haché
+              const sql =
+                "INSERT INTO users (nom, prenom, email, password) VALUES (?, ?, ?, ?)";
+              connection.query(
+                sql,
+                [nom, prenom, email, hashedPassword], // Utilisez le mot de passe haché
+                (insertErr, result) => {
+                  if (insertErr) {
+                    console.error(
+                      "Erreur lors de l'insertion de l'utilisateur dans la base de données :",
+                      insertErr
+                    );
+                    callback(insertErr, null);
+                  } else {
+                    callback(null, result);
+                  }
+                }
+              );
             }
-          );
-        }
+          }
+        );
       }
-    );
+    });
   },
   getUserByEmail: (email, callback) => {
     const sql = "SELECT * FROM users WHERE email = ?";
