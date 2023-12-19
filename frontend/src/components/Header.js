8f68e4204cce8354d67241d749eac4973e4ac91c
Talatizi <talatizikamel88@gmail.com>
2023-12-19 15:05:44 +0100

    mise a jour homepage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,16 @@
-import React, { useContext } from "react";
 import styles from "./Header.module.scss";
 import logo from "../assets/images/Logo-PZ-Bleu-Blanc-Rouge.png";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
 function Header() {
-  const { user, logout } = useContext(AuthContext);
-
-  const handleLogout = () => {
-    logout();
-  };
-
+  const { user } = useContext(AuthContext);
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
         <NavLink to="/">
-          <img src={logo} alt="logo planzone"></img>
+          {" "}
+          <img src={logo} alt="logo planzone"></img>{" "}
         </NavLink>
       </div>
       {user ? (
@@ -24,17 +18,16 @@ function Header() {
           <NavLink to="Profile">
             <button className="btn btn-primary mr-5">Profil</button>
           </NavLink>
-          <button
-            className="btn btn-reverse-primary ml-5"
-            onClick={handleLogout}
-          >
-            Déconnexion
-          </button>
+          <NavLink to="/">
+            <button className="btn btn-reverse-primary ml-5">
+              Déconnexion
+            </button>
+          </NavLink>
         </ul>
       ) : (
         <ul>
           <NavLink to="Login">
-            <button className="btn btn-primary mr-5">Connexion</button>
+            <button className="btn btn-primary mr-5">connexion</button>
           </NavLink>
           <NavLink to="signup">
             <button className="btn btn-reverse-primary ml-5">
--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,20 +1,25 @@
 import React from "react";
+import image from "../assets/images/image.jpg";
+import style from "./Homepage.module.scss";
 
 function Homepage() {
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-8 text-center">
-          <h1 className="display-4 mb-4">
-            Bienvenue dans notre site Todo List
-          </h1>
-          <p className="lead">
-            Gérez vos tâches quotidiennes de manière simple et efficace.
-          </p>
+    <div class=" d-flex container align-items-center">
+      <div class=" d-flex container justify-content-center align-items-center card my-30">
+        <div class={`${style.box} card`}>
+          <h1>Bienvenue dans notre site Todo List</h1>
+          <p>Gérez vos tâches quotidiennes de manière simple et efficace.</p>
           <p>
             Notre application Todo List vous permet de rester organisé et de
             suivre vos progrès.
           </p>
+        </div>
+        <div class={`${style.box} card`}>
+          <img
+            src={image}
+            alt="Illustration Todo List"
+            className="resized-image justify-content-d  my-30"
+          />
         </div>
       </div>
     </div>
